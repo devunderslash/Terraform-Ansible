55c2af941353aa6ad9785ea0407d59b48797f0f9
Andreas <panteliandreas@gmail.com>
2020-01-29 08:24:58 +0200

    add ssh in outputs

--- artifacts/terraform/outputs.tf
+++ artifacts/terraform/outputs.tf
@@ -6,3 +6,10 @@ output "url-gitLab" {
   value = "http://${aws_instance.gitLab[0].public_ip}"
 }
 
+output "ssh-jenkins" {
+  value = "ssh -i ${var.private_key} ${var.ansible_user}@${aws_instance.jenkins-ci[0].public_ip}"
+}
+
+output "ssh-gitLab" {
+  value = "ssh -i ${var.private_key} ${var.ansible_user}@${aws_instance.gitLab[0].public_ip}"
+}
